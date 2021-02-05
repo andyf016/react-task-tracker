2a73ed584d64617e4e2348d126374118191564e5
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-05 01:34:23 -0500

    working with heading and css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import Header from './components/Header'
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="container">
+     
+      <Header />
     </div>
   );
 }
+
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+const Header = ({title}) => {
+    return (
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: "Task Tracker"
+}
+// CSS in JS
+// const headingStyle = {
+//     color: 'red',
+//    backgroundColor: 'black'
+// }
+
+export default Header
