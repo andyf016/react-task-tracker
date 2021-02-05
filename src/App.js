0e25be8f8848fcad4ab30a12530ecded4dc322eb
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-05 02:26:11 -0500

    tasks rendering properly

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import {useState} from 'react'
 import Header from './components/Header'
-
+import Tasks from './components/Tasks'
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Doctor',
+      day: 'Feb 5th at 2:32',
+      reminder: true
+    },
+    {
+      id: 2,
+      text: 'Dentist',
+      day: 'Feb 6th at 8:32',
+      reminder: true
+    },
+    {
+      id: 3,
+      text: 'School',
+      day: 'Feb 8th at 2:52',
+      reminder: false
+    }
+  ])
+
   return (
     <div className="container">
      
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
