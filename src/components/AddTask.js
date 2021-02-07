7bdc132b56ac2f33e0e68b3bec6427aa36cad80c
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-06 23:21:57 -0500

    working on addtask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -4,8 +4,16 @@ const AddTask = () => {
     return (
         <form classname='add-form'>
             <div className='form-control'>
-            <label>Task</label>
-            <input type='text' placeholder='Add Task'/>
+                <label>Task</label>
+                <input type='text' placeholder='Add Task'/>
+            </div>
+            <div className='form-control'>
+                <label>Day and Time</label>
+                <input type='text' placeholder='Add Day and Time'/>
+            </div>
+            <div className='form-control'>
+                <label>Set Reminder</label>
+                <input type='checkbox' placeholder='Add Task'/>
             </div>
         </form>
     )
