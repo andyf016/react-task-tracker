e67cd01a7312135f3bdb70166a7388128c2aea6c
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-07 11:48:54 -0500

    fix issue with show hide form

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types'
-
-const Button = ({color, text}) => {
+const onClick = () =>{
+    
+}
+const Button = ({color, text, onClick}) => {
     return (
-        <button style={{backgroundColor: color}} className='btn'>{text}</button>
+        <button onClick={onClick} style={{backgroundColor: color}} className='btn'>{text}</button>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import Button from './Button'
 
 const Header = ({title, onAdd}) => {
+
     return (
         <header className='header'>
             <h1>{title}</h1>
