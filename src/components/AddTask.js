1834d84d9a8c44d43b2eec30fdb178c900d581a3
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-06 23:36:56 -0500

    add onchange to addtask

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
+
 
 const AddTask = () => {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
     return (
         <form className='add-form'>
             <div className='form-control'>
                 <label>Task</label>
-                <input type='text' placeholder='Add Task'/>
+                <input type='text' placeholder='Add Task' value={text} onChange={(e) => setText(e.target.value)}/>
             </div>
             <div className='form control form-control-check'>
                 <label>Day and Time</label>
-                <input type='text' placeholder='Add Day and Time'/>
+                <input type='text' placeholder='Add Day and Time' value={day} onChange={(e) => setDay(e.currentTarget.checked)}/>
             </div>
             <div className='form-control'>
                 <label>Set Reminder</label>
-                <input type='checkbox' />
+                <input type='checkbox' value={reminder} onChange={(e) => setReminder(e.target.value)}/>
             </div>
 
             <input type='submit' value='Save Task' className='btn btn-block' />
