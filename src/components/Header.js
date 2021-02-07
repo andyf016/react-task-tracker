69789d04cb0935f1a97e3cf2f3f641df51059a70
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-06 23:58:20 -0500

    working on toggle view

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import Button from './Button'
 
-const Header = ({title}) => {
+const Header = ({title, onAdd}) => {
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add'/>
+            <Button color='green' text='Add' onClick={onAdd}/>
         </header>
     )
 }
