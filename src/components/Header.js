315082685cc8095d98454118ceb54c88f5cba6d5
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-08 13:08:09 -0500

    implement useLocation hook for add button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,27 @@
-import Button from './Button'
+import { useLocation } from "react-router-dom";
+import Button from "./Button";
 
-const Header = ({title, onAdd, showAdd}) => {
-
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            <Button color={showAdd ? 'Red' : 'Green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>
-        </header>
-    )
-}
+const Header = ({ title, onAdd, showAdd }) => {
+    const location = useLocation()
+  return (
+    <header className='header'>
+      <h1>{title}</h1>
+      {location.pathname === '/' && <Button
+        color={showAdd ? "Red" : "Green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />}
+    </header>
+  );
+};
 
 Header.defaultProps = {
-    title: "Task Tracker"
-}
+  title: "Task Tracker",
+};
 // CSS in JS
 // const headingStyle = {
 //     color: 'red',
 //    backgroundColor: 'black'
 // }
 
-export default Header
+export default Header;
