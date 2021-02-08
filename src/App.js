4a72e3ce15fdc648a88d8fa58b7fc19fc4f1a767
Andrew Fillenwarth <andyf016@gmail.com>
2021-02-08 13:00:36 -0500

    finish routing

--- src/App.js
+++ src/App.js
@@ -82,12 +82,17 @@ function App() {
         onAdd={() => setShowAddTask(!showAddTask)}
         showAdd={showAddTask}
       />
-      {showAddTask && <AddTask onAdd={addTask} />}
+
+      <Route path='/' exact render={(props) => (
+          <>
+              {showAddTask && <AddTask onAdd={addTask} />}
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
       ) : (
         "No Tasks"
       )}
+          </>
+      )} />
       <Route path='/about' component={About} />
       <Footer />
     </div>
